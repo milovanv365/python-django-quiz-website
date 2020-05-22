15730c4aa0ba0f14e3f19c95074e693f24557bf6
fullstack <fullstack@email.com>
2020-05-22 21:41:57 +0100

    saved user settings to localstorage

--- quiz/views.py
+++ quiz/views.py
@@ -32,8 +32,8 @@ class QuizStepTwoView(View):
             commission_values.append(x)
 
         context = {
-            "sales_values": sales_values,
-            "commission_values": commission_values
+            'sales_values': sales_values,
+            'commission_values': commission_values
         }
         return render(request, template_name=self.template_url, context=context)
 
@@ -42,14 +42,48 @@ class QuizStepThreeView(View):
     template_url = 'quiz_step_three.html'
 
     def get(self, request):
-        return render(request, template_name=self.template_url)
+        distance_values = []
+        for x in range(1000, 26000, 1000):
+            distance_values.append(x)
+
+        data_usage_values = []
+        for x in range(1, 21, 1):
+            data_usage_values.append(x)
+
+        context = {
+            'distance_values': distance_values,
+            'data_usage_values': data_usage_values
+        }
+        return render(request, template_name=self.template_url, context=context)
 
 
 class QuizStepFourView(View):
     template_url = 'quiz_step_four.html'
 
     def get(self, request):
-        return render(request, template_name=self.template_url)
+
+        important_things = [
+            {'value': 'a great work environment / good colleagues', 'label': 'et supert arbeidsmiljø/gode kolleger'},
+            {'value': 'opportunity for personal development', 'label': 'mulighet for personlig utvikling'},
+            {'value': 'the best technology', 'label': 'den beste teknologien'},
+            {'value': 'opportunity for partnership', 'label': 'mulighet for partnerskap'},
+            {'value': 'high salary', 'label': 'høy lønn'},
+            {'value': 'freedom', 'label': 'frihet'},
+            {'value': 'flexibility', 'label': 'fleksibilitet'},
+            {'value': 'to be able to influence the workplace', 'label': 'å kunne påvirke arbeidsplassen'},
+            {'value': 'quality', 'label': 'kvalitet'},
+            {'value': 'safety', 'label': 'trygghet'},
+            {'value': 'to have back office', 'label': 'å ha backoffice'},
+            {'value': 'the best toolbox', 'label': 'den beste verktøykassen'},
+            {'value': 'winning culture', 'label': 'vinnerkultur'},
+            {'value': 'good lead approach', 'label': 'god leadstilgang'}
+        ]
+
+        context = {
+            'important_things': important_things
+        }
+
+        return render(request, template_name=self.template_url, context=context)
 
 
 class QuizStepFiveView(View):
