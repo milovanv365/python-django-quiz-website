b2034f9ab7bd8ef274a0c6c7e13477ded69fef13
fullstack <fullstack@email.com>
2020-05-22 09:45:43 +0100

    added quiz_step_three, quiz_step_four, quiz_step_five

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import IndexView, QuizStepOneView, QuizStepTwoView
+from .views import IndexView, QuizStepOneView, QuizStepTwoView, QuizStepThreeView, QuizStepFourView, QuizStepFiveView
 
 app_name = 'quiz'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('quiz/step_one', QuizStepOneView.as_view(), name='quiz_step_one'),
-    path('quiz/step_two', QuizStepTwoView.as_view(), name='quiz_step_two')
+    path('quiz/step_two', QuizStepTwoView.as_view(), name='quiz_step_two'),
+    path('quiz/step_three', QuizStepThreeView.as_view(), name='quiz_step_three'),
+    path('quiz/step_four', QuizStepFourView.as_view(), name='quiz_step_four'),
+    path('quiz/step_five', QuizStepFiveView.as_view(), name='quiz_step_five')
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -20,7 +20,40 @@ class QuizStepTwoView(View):
     template_url = 'quiz_step_two.html'
 
     def get(self, request):
-        context = {
+        sales_values = []
+        for x in range(0, 155, 5):
+            if x == 0:
+                sales_values.append(1)
+            else:
+                sales_values.append(x)
 
+        commission_values = []
+        for x in range(1000, 100000, 2500):
+            commission_values.append(x)
+
+        context = {
+            "sales_values": sales_values,
+            "commission_values": commission_values
         }
         return render(request, template_name=self.template_url, context=context)
+
+
+class QuizStepThreeView(View):
+    template_url = 'quiz_step_three.html'
+
+    def get(self, request):
+        return render(request, template_name=self.template_url)
+
+
+class QuizStepFourView(View):
+    template_url = 'quiz_step_four.html'
+
+    def get(self, request):
+        return render(request, template_name=self.template_url)
+
+
+class QuizStepFiveView(View):
+    template_url = 'quiz_step_five.html'
+
+    def get(self, request):
+        return render(request, template_name=self.template_url)
